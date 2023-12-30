30161a0edeb6cb82e0cbe9ef7930d841fa6ed093
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-30 08:34:12 -0300

    fix icons class

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Social() {
   return (
     <div className="home_social">
-        <a href="https://www.instagram.com/" className="home_social_icon" target="_blank">
+        <a href="https://www.instagram.com/" className="home_social-icon" target="_blank">
         <i className="uil uil-instagram"></i>
         </a>
-        <a href="https://api.whatsapp.com/send?phone=5554984289836&text=Ol%C3%A1,%20temos%20interesse%20em%20contratar%20voc%C3%AA." className="home_social_icon" target="_blank">
+        <a href="https://api.whatsapp.com/send?phone=5554984289836&text=Ol%C3%A1,%20temos%20interesse%20em%20contratar%20voc%C3%AA." className="home_social-icon" target="_blank">
         <i class="uil uil-whatsapp"></i>
         </a>
-        <a href="https://www.github.com/" className="home_social_icon" target="_blank">
+        <a href="https://www.github.com/" className="home_social-icon" target="_blank">
         <i className="uil uil-github"></i>
         </a>
     </div>
