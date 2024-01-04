f2486f20736a0da457fe421f1aeb6e8bf696b0d0
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-04 12:59:03 -0300

    skill componet created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import About from './components/about/About'
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
     <main className='main'>
       <Home/>
       <About/>
+      <Skills/>
     </main>
     </>
   );
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className='skills section' id='skills'>
+        <h2 className='section_title'>Skills</h2>
+        <span className='section_subtitle'>My technical level</span>
+
+    </section>
+  )
+}
+
+export default Skills
