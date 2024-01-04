639a3038b2f2ec1b0b0d068f2ac28841d1c020db
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-04 12:16:49 -0300

    about content

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Info = () => {
   return (
     <div className="about_info grid">
-      <div className="about_box about_icon">
-      <i class='bx bx-award'></i>
+      <div className="about_box ">
+      <i class='bx bx-award about_icon'></i>
+
         <h3 className="about_title">Experience</h3>
         <span className="about_subtitle">1 Year Study</span>
       </div>
@@ -12,7 +13,7 @@ const Info = () => {
       <div className="about_box ">
       <i class='bx bx-briefcase-alt about_icon'></i>
         <h3 className="about_title">Completed</h3>
-        <span className="about_subtitle">4</span>
+        <span className="about_subtitle">4 + projects</span>
       </div>
 
       <div className="about_box">
