c6d7a4a56a2a4056fee0721714be740e2d2be20a
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-04 19:20:24 -0300

    Backend skills content

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -2,7 +2,69 @@ import React from 'react'
 
 const Backend = () => {
   return (
-    <div>Backend</div>
+    <div className="skills_content">
+
+    <h3 className="skills_title">Backend developer</h3>
+
+     <div className='skills_box'>
+        <div className="skills_group">
+
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>PHP</h3>
+                <span className='skills_level'>Basic</span>
+            </div>
+            </div>
+
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>Bootstrap</h3>
+                <span className='skills_level'>Basic</span>
+            </div>
+            </div>
+
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>MySQL</h3>
+                <span className='skills_level'>Basic</span>
+            </div>
+            </div>
+
+        </div>
+        
+         <div className="skills_group">
+
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>Node.js</h3>
+                <span className='skills_level'>Basic</span>
+            </div>
+            </div>
+
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>MongoDB</h3>
+                <span className='skills_level'>Intermediate</span>
+            </div>
+            </div>
+            <div className="skills_data">
+                <i class="bx bx-badge-check"></i>
+            <div>
+                <h3 className='skills.name'>SQL</h3>
+                <span className='skills_level'>Intermediate</span>
+            </div>
+            </div>
+
+
+        </div>
+        
+    </div>
+</div>
   )
 }
 
