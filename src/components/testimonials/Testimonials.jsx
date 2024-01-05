f8853dd1a222cc0d6f5049cdd48f08a676bbd341
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-05 09:16:25 -0300

    created testimonial component

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <section className="testimonial container section">
+        <h2 className="section_title">My clients say</h2>
+        <span className="section_subtitle">Testimonial</span>
+
+
+        <div className="testimonial_container"></div>
+    </section>
+  )
+}
+
+export default Testimonials
