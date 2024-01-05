c65b7098b26f14f65bcf2de8f5afe73b3e3c6cda
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-05 13:01:36 -0300

    footer content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import "./footer.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <div className="footer_container container">
+            <h1 className="footer_title">Victor</h1>
+
+            <ul className="footer_list">
+                <li>
+                    <a href="#about" className="footer_link">About</a>
+                </li>
+                <li>
+                    <a href="#portfolio" className="footer_link">Projects</a>
+                </li>
+                <li>
+                    <a href="#testimonials" className="footer_link">Testimonials</a>
+                </li>
+            </ul>
+            <div className="footer_social">
+                <a href="https://www.instagram.com/" className="footer_social-link" target="_blank">
+                <i className="uil uil-instagram"></i>
+                </a>
+                <a href="https://www.youtube.com/@drayuuta9927" className="footer_social-link" target="_blank">
+                <i class="uil uil-youtube"></i>
+                </a>
+                <a href="https://www.github.com/" className="footer_social-link" target="_blank">
+                <i className="uil uil-github"></i>
+                </a>
+            </div>
+
+            <span className='footer_copy'>
+                &#169; Crypticalcoder. All rigths reserved
+            </span>
+
+        </div>
+    </footer>
   )
 }
 
