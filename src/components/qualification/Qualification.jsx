e6c7107076cc7e98fc1a763a5d2003ca9ed69a62
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-05 04:37:07 -0300

    qualification CSS

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -4,18 +4,19 @@ const Qualification = () => {
   return (
     <section className="qualification section">
         <h2 className="section_title">Qualification</h2>
-        <span className="section subtitle">My personal journey</span>
+        <span className="section_subtitle">My personal journey</span>
 
         <div className="qualification_container container">
-            <div className="qualification_active button--flex">
-                <i className="uil uil-graduation-cap qualification-icon"></i>
-                {" "}Education
+            <div className="qualification_tabs">
+                <div className="qualification_button qualification_active button--flex">
+                    <i className="uil uil-graduation-cap qualification_icon"></i>
+                    {" "}Education
+                </div>
+                <div className="qualification_button button--flex">
+                    <i className="uil uil-briefcase-alt qualification_icon"></i>
+                    {" "}Experience
+                </div>
             </div>
-            <div className="qualification_button button--flex">
-                <i className="uil uil-biefcase-alt qualification_icon"></i>
-                {" "}Experience
-            </div>
-
           
 
             <div className="qualification_sections">
@@ -26,7 +27,7 @@ const Qualification = () => {
                         <div>
                             <h3 className="qualification_title">Web Developement</h3>
                             <span className="qualification_subtitle">Uninter - Institute</span>
-                            <div className="qualification_calender">
+                            <div className="qualification_calendr">
                                 <i className="uil uil-calendar-alt"></i>
                                 2023 - Present
                             </div>
