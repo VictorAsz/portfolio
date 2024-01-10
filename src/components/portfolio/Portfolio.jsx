06b7f006266a89876f546cbe7d5b3527815280f8
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-10 02:46:32 -0300

    projects import

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './portfolio.css'
+import './portfolio.css';
+import Project from './Project';
 
 const Portfolio = () => {
   return (
     <section className="portfolio section">
         <h2 className="section_title">Portfolio</h2>
         <span className="section_subtitle">My recents projects</span>
+
+
+        <div className='projects_container container grid'>
+       <Project/>
+       <Project/>
+       <Project/>
+       <Project/>
+        </div>
+
+
     </section>
   )
 }
