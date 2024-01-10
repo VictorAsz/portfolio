6d1b593182897118b627e5d208792197acc1b588
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-10 02:46:12 -0300

    project modal component

---
+++ src/components/portfolio/Project.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './portfolio.css'
+
+const Project = () => {
+  return (
+    <div className='portfolio_content'>
+    <h3 className="portfolio_title">Project</h3>
+    
+
+    </div>
+
+  )
+}
+
+export default Project
