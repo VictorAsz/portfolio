be6bb7342298badefc77d55e12b95486242198b9
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-10 07:12:59 -0300

    change works button order

--- src/components/portfolio/Workitems.jsx
+++ src/components/portfolio/Workitems.jsx
@@ -6,13 +6,14 @@ const Workitems = ({item}) => {
         <img src={item.image} alt="" className='work_img' />
         <h3 className='work_title'>{item.title}</h3>
 
-        <button></button>
-        <button onclick="#" className="work_button">
-            Demo <i className="bx bx-right-arrow-alt work_button-icon"></i>
-        </button>
+      
         <button onclick="#" className="work_button github_button">
             Github <i className="uil uil-github work_github-icon"></i>
         </button>
+        <button onclick="#" className="work_button">
+            Demo <i className="bx bx-right-arrow-alt work_button-icon"></i>
+        </button>
+        
     </div>
   )
 }
