810690f36905feac0041ab0a780dc42f4dadbc99
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-10 05:29:27 -0300

    work items image, title and link

--- src/components/portfolio/Workitems.jsx
+++ src/components/portfolio/Workitems.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Workitems = () => {
+const Workitems = ({item}) => {
   return (
-    <div>Workitems</div>
+    <div className="work_card" key={item.id}>
+        <img src={item.image} alt="" className='work_img' />
+        <h3 className='work_title'>{item.title}</h3>
+        <a href="#" className="work_button">
+            Demo <i className="bx bx-right-arrow-alt work_button-icon"></i>
+        </a>
+    </div>
   )
 }
 
