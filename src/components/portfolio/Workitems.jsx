f871d9f52067ba965b5f43bd451249cc5b5acb55
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-11 19:09:25 -0300

    add itens  to  a

--- src/components/portfolio/Workitems.jsx
+++ src/components/portfolio/Workitems.jsx
@@ -7,7 +7,7 @@ const Workitems = ({item}) => {
         <h3 className='work_title'>{item.title}</h3>
 
       
-        <a href='https://github.com/VictorAsz/HDCevents' target='blank' className="work_button github_button">
+        <a href={item.githublink} target='blank' className="work_button github_button">
             Github <i className="uil uil-github work_github-icon"></i>
         </a>
         <a href={item.demo} className="work_button">
