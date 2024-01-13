27b81630449cd7f811ed4588e4cb4af2eaa5c51d
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-13 08:04:06 -0300

    Att portfolio projects

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,50 +1,68 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
+import HDCevents from "../../assets/HDCevents.jpg";
+import OLDportfolio from "../../assets/OLDportfolio.jpg";
+import MooseFilm from "../../assets/MooseFilm.jpg";
+import AttendanceList from "../../assets/AttendanceList.jpg";
 import Work5 from "../../assets/work5.jpg";
+import Instagram from "../../assets/Instagram.jpg";
 
 export const projectsData = [
     {
       id: 1,
-      image: Work1,
-      title: "HDC",
-      category: "Mobile",
+      image: HDCevents,
+      title: "HDC Events",
+      category: "Backend",
       demo: null,
       githublink: "https://github.com/VictorAsz/HDCevents",
     },
     {
       id: 2,
-      image: Work2,
-      title: "projeto 2",
-      category: "Backend",
+      image: OLDportfolio,
+      title: "Old Next.js Portfolio",
+      category: "Frontend",
       demo: "https://personal-portfolio-mu-puce.vercel.app/",
-      githublink: "#",
+      githublink: "https://github.com/VictorAsz/Personal-Portfolio",
     },
     {
       id: 3,
-      image: Work3,
-      title: "projeto 3",
-      category: "Backend",
-      demo: null,
-      githublink: "https://github.com/VictorAsz/HDCevents",
+      image: MooseFilm,
+      title: "Moose Film",
+      category: "Frontend",
+      demo: "https://moose-film.vercel.app/",
+      githublink: "https://github.com/VictorAsz/MooseFilm",
     },
     {
       id: 4,
-      image: Work4,
-      title: "projeto 4",
+      image: AttendanceList,
+      title: "Attendance List",
       category: "Frontend",
-      demo: null,
-      githublink: "https://github.com/VictorAsz/HDCevents",
+      demo: "https://lista-de-presen-a-rockseat-discover.vercel.app/",
+      githublink: "https://github.com/VictorAsz/AttendanceList",
     },
     {
       id: 5,
       image: Work5,
-      title: "projeto 5",
+      title: "Todo List",
       category: "Frontend",
-      demo: null,
-      githublink: "https://github.com/VictorAsz/HDCevents",
+      demo: "https://react-todo-seven-nu.vercel.app/",
+      githublink: "https://github.com/VictorAsz/Todo-List",
     },
+    {
+      id: 6,
+      image: Instagram,
+      title: "Instagram Clone(in progress...)",
+      category: "Backend",
+      demo: null,
+      githublink: "https://github.com/VictorAsz/Instagram-clone",
+    },
+    {
+      id: 7,
+      image: Work5,
+      title: "PHP CRUD with MySQL and Bootstrap",
+      category: "Backend",
+      demo: null,
+      githublink: "https://github.com/VictorAsz/CRUDPHP-BDmySQL",
+    },
+
   ];
   
   
@@ -58,7 +76,5 @@ export const projectsData = [
     {
         name: "Backend",
     },
-    {
-        name: "Mobile",
-    },
+  
   ]
