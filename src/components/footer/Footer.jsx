02951a453cab197b653ff33def7170047a1f9bdc
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-15 16:44:34 -0300

    começo da troca de idiomas

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
             <ul className="footer_list">
                 <li>
-                    <a href="#about" className="footer_link">About</a>
+                    <a href="#about" className="footer_link">Sobre</a>
                 </li>
                 <li>
                     <a href="#portfolio" className="footer_link">Projects</a>
